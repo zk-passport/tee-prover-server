218f767178ce5e740b5d10c8070cb7af12788f42
ayman <aymanshaik1015@gmail.com>
2025-02-13 19:11:06 +0530

    feat: listen to db params in the parent instance

--- start.sh
+++ start.sh
@@ -6,19 +6,20 @@ ip link set dev lo up
 socat VSOCK-LISTEN:8888,fork tcp-connect:127.0.0.1:8888,reuseaddr & # for the json rpc server
 socat tcp-listen:8889,fork vsock-connect:3:8889,reuseaddr & # for the db
 
+# assume that I get the db url string from the parent instance
+DB_PARAMS=$(socat -u vsock-listen:8890,reuseaddr - | head -n 1)
+
+DB_USER=$(echo "$DB_PARAMS" | sed -E 's#^postgres://([^:]+):.*#\1#')
+DB_PASS=$(echo "$DB_PARAMS" | sed -E 's#^postgres://[^:]+:([^@]+)@.*#\1#')
+DB_NAME=$(echo "$DB_PARAMS" | sed -E 's#^.*/([^/]+)$#\1#')
+
+DATABASE_URL="postgres://$DB_USER:$DB_PASS@127.0.0.2:8889/$DB_NAME"
+
+echo $DATABASE_URL
+
 ./usr/local/bin/tee-server \
     --server-address=127.0.0.1:8888 \
-    --database-url=postgres://postgres:passport@127.0.0.2:8889/openpassport \
+    --database-url=$DATABASE_URL \
     --circuit-folder=/circuits \
     --zkey-folder=/zkeys \
     --rapidsnark-path=/rapidsnark
-
-# For running locally
-# cargo run -- \
-#     --server-address=127.0.0.1:8888 \
-#     --database-url=postgres://postgres:mysecretpassword@127.0.0.1:5433/db \
-#     --circuit-folder=./circuits \
-#     --zkey-folder=./zkeys \
-#     --circuit-zkey-map register_sha1_sha256_sha256_rsa_65537_4096=register_sha1_sha256_sha256_rsa_65537_4096.zkey \
-#     --circuit-zkey-map register_sha256_sha256_sha256_ecdsa_brainpoolP256r1=register_sha256_sha256_sha256_ecdsa_brainpoolP256r1.zkey \
-#     --rapidsnark-path=./rapidsnark
